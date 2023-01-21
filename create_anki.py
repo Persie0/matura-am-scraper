54c68ab098b3448791f1aa460dc7d88de312acea
Persie0 <hashcod3@protonmail.com>
2023-01-21 23:40:54 +0100

    finished

--- create_anki.py
+++ create_anki.py
@@ -10,9 +10,9 @@ thema="Binomialverteilung"
 
 my_deck = genanki.Deck(
     deck_id=12345, # a unique id for the deck
-    name=thema) # the name of the deck
+    name=thema+"_RDP_scraped") # the name of the deck
 
-
+img=[]
 #filter by thema
 data = [i for i in data if i['thema'] == thema]
 c=0
@@ -28,7 +28,8 @@ for bsp in data:
     ab_path= auf_dir+"/"+ab_name+".png"
     my_card = genanki.Note(
     model=genanki.Model(
-        model_id=12345,
+        1380120064,
+  'Example',
         fields=[
             {'name': 'Image1'},
             {'name': 'Image2'},
@@ -39,17 +40,26 @@ for bsp in data:
         templates=[
             {
                 'name': 'Card 1',
-                'qfmt': '{{Image}}<br>{{Image}}',
-                'afmt': '{{Image}}<br>{{Answer}}',
+                'qfmt': '{{Image1}}<br>{{Image2}}',
+                'afmt': '{{AnswerImage}}<br>{{Answer}}',
             },
         ]
     ),
-    fields=[ah_path, ab_path, auf_dir+"/"+"loesung.jpeg", bsp["loesung"]])
+    fields=['<img src="{}">'.format(ah_name+".png"), '<img src="{}">'.format(ab_name+".png"), '<img src="{}">'.format("loesung.jpeg"), '<a href="{}">Link</a>'.format(bsp["loesung"])])
+
+    img.append(ah_path)
+    img.append(ab_path)
+    img.append(auf_dir+"/"+"loesung.jpeg")
     my_deck.add_note(my_card)
-    if c==4:
+    if c==3:
         break
 
-genanki.Package(my_deck).write_to_file(thema+'.apkg')
+my_package = genanki.Package(my_deck)
+my_package.media_files = img
+
+
+
+my_package.write_to_file(thema+'.apkg')
 
 
 
